5d9fb22b052b0324000380d55a296900bee92164
rk134 <spaced@rk134.cf>
2022-10-18 18:27:49 +0100

    spaced: vendorsetup: vt-ims: End if statement correctly
    Signed-off-by: rk134 <spaced@rk134.cf>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,7 @@ fi
 # Vendor-ims tree
 VT_IMS=vendor/realme/ims-spaced/mtk-ims.mk
 if ! [ -a $VT_IMS ]; then git clone https://github.com/drtsinx92/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced
+fi
 
 # Kernel tree
 KT=kernel/realme/spaced/build.sh
@@ -15,11 +16,11 @@ fi
 # frameworks/av (required for sysbta)
 cd frameworks/av
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH" != "13-sysbta"]; then cd ../.. && git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
+if ! [ "$BRANCH" != "13-sysbta" ]; then cd ../.. && git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
 fi
 
 # packages/modules/Bluetooth (required for sysbta)
 cd packages/modules/Bluetooth
 BRANCH_BT="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH_BT" != "13-sysbta"]; then cd ../../.. && git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
+if ! [ "$BRANCH_BT" != "13-sysbta" ]; then cd ../../.. && git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
 fi
