e6c464d582ca72f936ffd3cd1fbdb6bfcd7eb1d4
rk134 <spaced@rk134.cf>
2022-10-19 10:24:36 +0100

    spaced: vendorsetup: rework script
    Signed-off-by: rk134 <spaced@rk134.cf>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,29 +1,20 @@
 # Vendor tree
-VT=vendor/realme/spaced/spaced-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/drtsinx98/proprietary_vendor_realme_spaced vendor/realme/spaced --depth=1
-fi
+git clone https://github.com/drtsinx98/proprietary_vendor_realme_spaced vendor/realme/spaced --depth=1
+
 
 # Vendor-ims tree
-VT_IMS=vendor/realme/ims-spaced/mtk-ims.mk
-if ! [ -a $VT_IMS ]; then git clone https://github.com/drtsinx98/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced --depth=1
-fi
+git clone https://github.com/drtsinx98/proprietary_vendor_realme_ims-spaced vendor/realme/ims-spaced --depth=1
+
 
 # Kernel tree
-KT=kernel/realme/spaced/build.sh
-if ! [ -a $KT ]; then git clone --depth=1 https://github.com/drtsinx98/android_kernel_realme_mt6781 kernel/realme/spaced
-fi
+git clone --depth=1 https://github.com/drtsinx98/android_kernel_realme_mt6781 kernel/realme/spaced
 
 # frameworks/av (required for sysbta)
-cd frameworks/av
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH" != "13-sysbta" ]; then cd ../.. && git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
-fi
+git clone --depth=1 https://github.com/rk134/frameworks_av.git frameworks/av
+
 
 # packages/modules/Bluetooth (required for sysbta)
-cd packages/modules/Bluetooth
-BRANCH_BT="$(git rev-parse --abbrev-ref HEAD)"
-if ! [ "$BRANCH_BT" != "13-sysbta" ]; then cd ../../.. && git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
-fi
+git clone --depth=1 https://github.com/rk134/packages_modules_Bluetooth.git packages/modules/Bluetooth
 
 # Clang 11
 git clone https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-r383902 --depth=1
